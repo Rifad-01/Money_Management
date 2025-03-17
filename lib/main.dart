9e5534407e48c4371ae45c688331c620635c265b
Mohammed Rifad <rifadferoke01@gmail.com>
2025-03-17 22:44:59 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const MoneyManager());
+}
+
+class MoneyManager extends StatelessWidget {
+  const MoneyManager({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
